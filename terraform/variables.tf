3f80c53d45170520191dc0103c611f935d0a32bb
Pakthapriyan <105651844+Pakthapriyan@users.noreply.github.com>
2025-12-15 16:30:38 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,26 @@ variable "aws_region" {
   default = "eu-north-1"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_access_key" {}
+variable "aws_access_key" {
+  sensitive = true
+}
 
-variable "image_uri" {}
+variable "aws_secret_access_key" {
+  sensitive = true
+}
 
-variable "app_keys" {}
-variable "api_token_salt" {}
-variable "admin_jwt_secret" {}
+variable "ecr_image_uri" {
+  description = "ECR image URI with tag"
+}
+
+variable "app_keys" {
+  default = "dummy1,dummy2"
+}
+
+variable "api_token_salt" {
+  default = "dummy_salt"
+}
+
+variable "admin_jwt_secret" {
+  default = "dummy_admin"
+}
