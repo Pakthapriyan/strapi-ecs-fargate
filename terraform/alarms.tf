b269845fad96154230af2c41027480d4f5f76358
Pakthapriyan <105651844+Pakthapriyan@users.noreply.github.com>
2025-12-17 13:21:38 +0530

    Create alarms.tf

---
+++ terraform/alarms.tf
@@ -0,0 +1,34 @@
+resource "aws_cloudwatch_metric_alarm" "ecs_memory_high" {
+  alarm_name          = "ecs-strapi-high-memory"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.strapi.name
+    ServiceName = aws_ecs_service.strapi.name
+  }
+
+  alarm_description = "High memory usage on Strapi ECS service"
+}
+resource "aws_cloudwatch_metric_alarm" "ecs_cpu_high" {
+  alarm_name          = "ecs-strapi-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.strapi.name
+    ServiceName = aws_ecs_service.strapi.name
+  }
+
+  alarm_description = "High CPU usage on Strapi ECS service"
+}
