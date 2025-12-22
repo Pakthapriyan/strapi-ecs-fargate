c172daf972034ec9c89373d763613e4a91bc3939
Pakthapriyan <105651844+Pakthapriyan@users.noreply.github.com>
2025-12-22 12:06:21 +0530

    Update dashboard.tf

--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -1,23 +1,19 @@
 resource "aws_cloudwatch_dashboard" "strapi" {
-  dashboard_name = "paktha-strapi-ecs-dashboard"
+  dashboard_name = "paktha-strapi-dashboard"
 
   dashboard_body = jsonencode({
     widgets = [
-
-      # ECS CPU Utilization
       {
         type   = "metric"
         x      = 0
         y      = 0
         width  = 12
         height = 6
-
         properties = {
           title  = "ECS CPU Utilization"
           region = var.aws_region
           stat   = "Average"
           period = 60
-
           metrics = [
             [
               "AWS/ECS",
@@ -28,20 +24,17 @@ resource "aws_cloudwatch_dashboard" "strapi" {
           ]
         }
       },
-      # ECS Memory Utilization
       {
         type   = "metric"
         x      = 12
         y      = 0
         width  = 12
         height = 6
-
         properties = {
           title  = "ECS Memory Utilization"
           region = var.aws_region
           stat   = "Average"
           period = 60
-
           metrics = [
             [
               "AWS/ECS",
@@ -52,21 +45,17 @@ resource "aws_cloudwatch_dashboard" "strapi" {
           ]
         }
       },
-
-      # Task Count (Running vs Desired)
       {
         type   = "metric"
         x      = 0
         y      = 6
         width  = 12
         height = 6
-
         properties = {
-          title  = "Task Count (Running vs Desired)"
+          title  = "Running vs Desired Tasks"
           region = var.aws_region
           stat   = "Average"
           period = 60
-
           metrics = [
             [
               "AWS/ECS",
@@ -77,54 +66,6 @@ resource "aws_cloudwatch_dashboard" "strapi" {
             [
               "AWS/ECS",
               "DesiredTaskCount",
-              "ClusterName", aws_ecs_cluster.strapi.name,
-              "ServiceName", aws_ecs_service.strapi.name
-            ]
-          ]
-        }
-      },
-      # Network In (Bytes)
-      {
-        type   = "metric"
-        x      = 12
-        y      = 6
-        width  = 12
-        height = 6
-
-        properties = {
-          title  = "Network In (Bytes)"
-          region = var.aws_region
-          stat   = "Sum"
-          period = 60
-
-          metrics = [
-            [
-              "AWS/ECS",
-              "NetworkRxBytes",
-              "ClusterName", aws_ecs_cluster.strapi.name,
-              "ServiceName", aws_ecs_service.strapi.name
-            ]
-          ]
-        }
-      },
-      # Network Out (Bytes)
-      {
-        type   = "metric"
-        x      = 0
-        y      = 12
-        width  = 12
-        height = 6
-
-        properties = {
-          title  = "Network Out (Bytes)"
-          region = var.aws_region
-          stat   = "Sum"
-          period = 60
-
-          metrics = [
-            [
-              "AWS/ECS",
-              "NetworkTxBytes",
               "ClusterName", aws_ecs_cluster.strapi.name,
               "ServiceName", aws_ecs_service.strapi.name
             ]
