93eddb23cd2f16f2d565814d7cfda45f5bcc196a
Pakthapriyan <105651844+Pakthapriyan@users.noreply.github.com>
2025-12-22 17:36:45 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -95,9 +95,10 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.strapi.arn
+    target_group_arn = data.aws_lb_target_group.strapi.arn
   }
 }
+
 
 ################################
 # ECS TASK DEFINITION (UNCHANGED LOGIC)
