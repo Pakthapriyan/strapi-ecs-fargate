b6d5154ad2f8074ef99cbd8f643a574dd2e9c781
Pakthapriyan <105651844+Pakthapriyan@users.noreply.github.com>
2025-12-23 12:29:31 +0530

    Update codedeploy.tf

--- terraform/codedeploy.tf
+++ terraform/codedeploy.tf
@@ -55,3 +55,22 @@ resource "aws_codedeploy_deployment_group" "strapi" {
     }
   }
 }
+resource "aws_iam_role" "codedeploy" {
+  name = "paktha-codedeploy-ecs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy" {
+  role       = aws_iam_role.codedeploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
