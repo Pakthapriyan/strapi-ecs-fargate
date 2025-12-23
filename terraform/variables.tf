2233c6bc4c04a753494f3b270d371e9a22746054
Pakthapriyan <105651844+Pakthapriyan@users.noreply.github.com>
2025-12-23 12:29:57 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,6 @@ variable "api_token_salt" {
   type        = string
   default     = "dummy_salt"
 }
-
 variable "admin_jwt_secret" {
   description = "Strapi admin JWT secret"
   type        = string
@@ -29,4 +28,8 @@ variable "jwt_secret" {
   description = "JWT secret for users-permissions plugin"
   type        = string
   default     = "dummy_jwt_secret_change_me"
+}
+variable "codedeploy_role_arn" {
+  description = "IAM role ARN for CodeDeploy ECS"
+  type        = string
 }
