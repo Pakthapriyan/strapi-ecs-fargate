98f2c2dad75f8845a205a5e2d9fb0753bd45d683
Pakthapriyan <105651844+Pakthapriyan@users.noreply.github.com>
2025-12-23 12:28:59 +0530

    Create codedeploy.tf

---
+++ terraform/codedeploy.tf
@@ -0,0 +1,57 @@
+# CODEDEPLOY APPLICATION
+
+resource "aws_codedeploy_app" "strapi" {
+  name             = "paktha-strapi-codedeploy"
+  compute_platform = "ECS"
+}
+
+# CODEDEPLOY DEPLOYMENT GROUP
+
+resource "aws_codedeploy_deployment_group" "strapi" {
+  app_name              = aws_codedeploy_app.strapi.name
+  deployment_group_name = "paktha-strapi-dg"
+  service_role_arn      = var.codedeploy_role_arn
+
+  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
+  # You can switch to:
+  # CodeDeployDefault.ECSAllAtOnce
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  blue_green_deployment_config {
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.strapi.name
+    service_name = aws_ecs_service.strapi.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.http.arn]
+      }
+
+      target_group {
+        name = aws_lb_target_group.blue.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.green.name
+      }
+    }
+  }
+}
